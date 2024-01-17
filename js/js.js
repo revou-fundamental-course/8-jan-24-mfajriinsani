b2e9fdc2780a091970b679c4952eb5cc2a7d916c
mfajriinsani <muhammadfajriinsani@gmail.com>
2024-01-17 21:14:27 +0700

    last push

--- js/js.js
+++ js/js.js
@@ -1,16 +1,37 @@
-let sisi = document.getElementById('sisi');
-let luas = document.getElementById('luas');
-let out1 = document.getElementById('output1');
+function input(){
+    let input = document.getElementById('input');
+    let tvalue = input.value;
 
-function fun1() {
-    out1.innerHTML = sisi.value;
+    let output = document.getElementById('output');
+    output.innerText = "L = " +tvalue+ " x " +tvalue;
 }
-luas.addEventListener('click',fun1);
+
+function result(){
+    let input = document.getElementById('input');
+    let tvalue = input.value;
+    let result = document.getElementById("result");
+    result.innerHTML = "Luas = "+tvalue * tvalue
+}
 
 function reset() {
-    let btnclear = document.querySelector('reset');
-    let inputs = document.querySelectorAll('input');
-    btnclear.addEventListener('click',()=>{
-        inputs.forEach(input=>input.value ='');
-    });
+    document.getElementById("input").value = "";
+} 
+
+function input2(){
+    let input2 = document.getElementById('input2');
+    let tvalue2 = input2.value;
+
+    let output2 = document.getElementById('output2');
+    output2.innerText = "K = 4 x" +tvalue2;
+}
+
+function result2(){
+    let input2 = document.getElementById('input2');
+    let tvalue2 = input2.value;
+    let result2 = document.getElementById("result2");
+    result2.innerHTML = "Keliling = "+ 4 * tvalue2
+}
+
+function reset2() {
+    document.getElementById("output2").value = "";
 } 
